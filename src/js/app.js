038ec71665e4e2a54420dabffeea309e00716d83
Michael Lefebvre <michael@scenedata.com>
2015-09-30 01:57:50 +0200

    tes browserify

--- src/js/app.js
+++ src/js/app.js
@@ -5,11 +5,12 @@ global.navigator = window.navigator
 var React       = require('react')
   , Gui         = window.require('nw.gui')
   , Squid       = require('squid-core')
-  , GHapp       = require('./github.json')
+  , GHapp       = require('../../github.json')
 
 // Setup core config
 Squid.setup({
-    config:   {
+    config: require('../../node_modules/squid-core/lib/config/core')
+  , locale:   {
         github: {
             credentials: {
                 client_id:     GHapp.client_id
@@ -18,10 +19,13 @@ Squid.setup({
         }
       , logger: false
     }
-  , env: 'dev'
+  , envName: 'dev'
 })
 
 console.log( Squid._VERSION )
+console.log( Squid._UID)
+// console.log( Squid.storage())
+Gui.Window.get().showDevTools()
 console.log( Squid.getConfig() )
 
 var HelloMessage = React.createClass({
@@ -34,9 +38,9 @@ React.render(<HelloMessage name="John" />,  document.getElementById('squid-app')
 
 var Gui     = window.require('nw.gui')
   , Win     = Gui.Window.get()
-
-var tmpTray = new Tray()
-console.log(tmpTray)
+var Tray = require('utils/tray')
+  , uTray = new Tray()
+console.log(uTray)
 
   // Minimal Menu bar item
   var nativeMenuBar = new Gui.Menu({ type: 'menubar' })
@@ -45,7 +49,8 @@ console.log(tmpTray)
 
   Win.menu = nativeMenuBar
 
-tmpTray.get().on( 'click', function()
+uTray.get().on( 'click', function()
 {
   console.log('tray ok')
+  Gui.Window.open('new.html')
 })
