5a873161f8604b144e309bb01e7e18d71e9f8cc3
Michael Lefebvre <michael@scenedata.com>
2015-10-12 01:40:06 +0200

    Test call store from app window

--- src/js/windows/main.js
+++ src/js/windows/main.js
@@ -7,12 +7,40 @@
 'use strict';
 
 var Desktop = window.Squid
+  , Core    = Desktop.core()
 
 var MainWindow = function()
 {
   console.log('+++++++')
   console.log( Desktop._UID )
   console.log( screen.height )
+
+
+  // set user token
+  // --------------------
+  var Config = require('../config') // private config file
+  Core.setGithubToken( Config.githubApp.token )
+
+  console.log( Core.getGithubToken() )
+
+  var store = Core.store('user')
+
+  store.listen( function( status )
+  {
+    if( status.user )
+    {
+      // console.log('------------------')
+      // console.log( store.model().attributes )
+      // console.log('------------------')
+      console.log(store.model().getName())
+      // userRepo()
+      // userOrgs()
+    }
+
+    if( status.error )
+      console.log('status: ', status.message)
+  })
+
   return this
 }
 
